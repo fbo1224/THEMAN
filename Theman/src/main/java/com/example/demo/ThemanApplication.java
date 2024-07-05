ab8e315a431ad1a6f2e7152512f6f0b587595f10
SeoJunHyeong <110711429+JunHyeongSeo@users.noreply.github.com>
2024-07-05 17:42:39 +0900

    12

---
+++ Theman/src/main/java/com/example/demo/ThemanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemanApplication.class, args);
+	}
+
+}
