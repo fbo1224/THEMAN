c7734ef40945c67a788a2fc6134afd0e24d1cb09
fbo1224 <161474075+fbo1224@users.noreply.github.com>
2024-07-19 18:07:16 +0900

    소셜 수정2

--- Theman/src/main/java/com/the/man/member/controller/SocialLoginController.java
+++ Theman/src/main/java/com/the/man/member/controller/SocialLoginController.java
@@ -6,6 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SocialLoginController {
 	
+	@GetMapping("login")
+	public String login() {
+		return "member/kakao-login";
+	}/*
+	
+	@GetMapping("kakao")
+	public String kakaoLogin() {
+		return "member/kakao-login";
+	}*/
+	
 	// Redirect URI
 	@GetMapping("code")
 	public String code() {
