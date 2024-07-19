da54f351267d8d9ca4c28a7b29f467fc41de96ea
SeoJunHyeong <110711429+JunHyeongSeo@users.noreply.github.com>
2024-07-19 16:23:59 +0900

    product

--- Theman/src/main/java/com/the/man/product/controller/ProductController.java
+++ Theman/src/main/java/com/the/man/product/controller/ProductController.java
@@ -2,11 +2,15 @@ package com.the.man.product.controller;
 
 import org.springframework.stereotype.Controller;
 
+import com.the.man.product.model.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class ProductController {
+	
+	
+	private ProductService productService;
 
 }
---
+++ Theman/src/main/java/com/the/man/product/model/service/ProductService.java
@@ -0,0 +1,5 @@
+package com.the.man.product.model.service;
+
+public interface ProductService {
+
+}
