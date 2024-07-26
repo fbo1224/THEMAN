9cdbd10c2148136b08da6ca652cf4a8723d48470
SeoJunHyeong <110711429+JunHyeongSeo@users.noreply.github.com>
2024-07-26 19:10:32 +0900

    insert

--- Theman/src/main/java/com/the/man/product/model/service/ProductServiceImpl.java
+++ Theman/src/main/java/com/the/man/product/model/service/ProductServiceImpl.java
@@ -1,5 +1,22 @@
 package com.the.man.product.model.service;
 
+import org.springframework.stereotype.Service;
+
+import com.the.man.product.model.repository.ProductMapper;
+import com.the.man.product.model.vo.Product;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService{
+	
+	private final ProductMapper productMapper;
+
+	@Override
+	public int insert(Product product) {
+		// TODO Auto-generated method stub
+		return productMapper.insert(product);
+	}
 
 }
